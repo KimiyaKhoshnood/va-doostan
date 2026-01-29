09ff6cb5a26c9d2d06dae761b9215e72f2f2515c
KimiyaKhoshnood <kimiya.khoshnod@gmail.com>
2026-01-29 17:38:22 +0330

    Deleted backend

--- backend/global.d.ts
+++
@@ -1,8 +0,0 @@
-declare namespace NodeJS {
-  interface ProcessEnv {
-    URI_Mongodb: string,
-    JWT_SECRET: string,
-    JWT_REFRESH_SECRET: string,
-    JWT_EXPIRES_IN: StringValue | number
-  }
-}
